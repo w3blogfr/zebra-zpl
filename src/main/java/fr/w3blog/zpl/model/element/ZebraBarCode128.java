553edb176b800e4a96f159a785daa282c6c34b76
Matthias Vets <matthias.vets@pearlchain.net>
2014-10-23 14:28:32 +0200

    implementation for BarCode 128

---
+++ src/main/java/fr/w3blog/zpl/model/element/ZebraBarCode128.java
@@ -0,0 +1,58 @@
+package fr.w3blog.zpl.model.element;
+
+import fr.w3blog.zpl.model.PrinterOptions;
+import fr.w3blog.zpl.utils.ZplUtils;
+
+/**
+ * Element to create a bar code 128
+ * 
+ * Zpl command : ^BC
+ * 
+ * @author matthiasvets
+ * 
+ */
+public class ZebraBarCode128 extends ZebraBarCode {
+
+	private boolean checkDigit43 = false;
+
+	public ZebraBarCode128(int positionX, int positionY, String text, int barCodeHeigth) {
+		super(positionX, positionY, text, barCodeHeigth);
+	}
+
+	public ZebraBarCode128(int positionX, int positionY, String text, int barCodeHeigth, int barCodeWidth, int wideBarRatio) {
+		super(positionX, positionY, text, barCodeHeigth, barCodeWidth, wideBarRatio);
+	}
+	
+	public ZebraBarCode128(int positionX, int positionY, String text, int barCodeHeigth, boolean showTextInterpretation, int barCodeWidth, int wideBarRatio) {
+        super(positionX, positionY, text, barCodeHeigth, showTextInterpretation, barCodeWidth, wideBarRatio);
+    }
+
+	public ZebraBarCode128(int positionX, int positionY, String text, int barCodeHeigth, int barCodeWidth, int wideBarRatio, boolean checkDigit43) {
+		super(positionX, positionY, text, barCodeHeigth, barCodeWidth, wideBarRatio);
+		this.setCheckDigit43(checkDigit43);
+	}
+
+	public ZebraBarCode128(int positionX, int positionY, String text, int barCodeHeigth, boolean showTextInterpretation, boolean showTextInterpretationAbove) {
+		super(positionX, positionY, text, barCodeHeigth, showTextInterpretation, showTextInterpretationAbove);
+	}
+
+	@Override
+	public String getZplCode(PrinterOptions printerOptions) {
+		StringBuilder zpl = getStartZplCodeBuilder();
+		zpl.append(ZplUtils.zplCommandSautLigne("BC", zebraRotation.getLetter(), barCodeHeigth, checkDigit43, showTextInterpretation, showTextInterpretationAbove));
+		zpl.append("^FD");
+		zpl.append(text);
+		zpl.append(ZplUtils.zplCommandSautLigne("FS"));
+		return zpl.toString();
+	}
+
+	public boolean isCheckDigit43() {
+		return checkDigit43;
+	}
+
+	public ZebraBarCode128 setCheckDigit43(boolean checkDigit43) {
+		this.checkDigit43 = checkDigit43;
+		return this;
+	}
+
+}
